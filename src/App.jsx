62f7db1d15385fffea1db52a5126d82ee4835374
RuslanGon <ruslan441006@gmail.com>
2024-06-04 11:34:20 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 
 import './App.css'
 import MailBox from './components/MailBox/MailBox'
-
-const MeestExpressUsers = [
-  {id:'1', userEmail: 'Ruslan@Gmail.com', userName: 'Ruslan'},
-  {id:'2', userEmail: 'Ira@gmail.com', userName: 'Ira'},
-  {id:'3', userEmail: 'Lena@gmail.com', userName: 'Lena'},
-]
-
-const NovaPoshta =[
-  {id:'4', userEmail: 'Roma@Gmail.com', userName: 'Roma'},
-  {id:'5', userEmail: 'Igor@gmail.com', userName: 'Igor'},
-  {id:'6', userEmail: 'Lena@gmail.com', userName: 'Lena'},
-]
-
-const UkrPoshta = [
-  {id:'7', userEmail: 'Ola@Gmail.com', userName: 'Ola'},
-  {id:'8', userEmail: 'Marina@gmail.com', userName: 'Marina'},
-  {id:'9', userEmail: 'Gena@gmail.com', userName: 'Gena'},
-]
+import MeestExpressUsers from '../meestExpress.json'
+import NovaPoshta from '../novaPoshta.json'
+import UkrPoshta from '../ukrPoshta.json'
 
 
 function App() {

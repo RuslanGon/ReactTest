223f7f07b83f2b1b8728321ed96c32327d288d5b
RuslanGon <ruslan441006@gmail.com>
2024-06-04 11:18:37 +0300

    wer

--- src/components/MailBox/MailBox.jsx
+++ src/components/MailBox/MailBox.jsx
@@ -5,7 +5,7 @@ const MailBox = ({boxtitle, mailBoxCount, boxUsers}) => {
       <h2>{boxtitle}</h2>
       {mailBoxCount === 0 ? <p>Сейчас нет активных ячеек</p> : <p>Количество активных ячеек: {mailBoxCount}</p> }
       <ul>
-        {boxUsers.map(user => {
+        {Array.isArray(boxUsers) && boxUsers.map(user => {
         return <li key={user.id}>{user.userEmail}</li>
         })}
       </ul>

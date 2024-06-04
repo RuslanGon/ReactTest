5bfc0ebd7e32c88a8e3b0293b6c992ca2e1c0df9
RuslanGon <ruslan441006@gmail.com>
2024-06-04 11:12:32 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@
 import './App.css'
 import MailBox from './components/MailBox/MailBox'
 
+const MeestExpressUsers = [
+  {id:'1', userEmail: 'Ruslan@Gmail.com', userName: 'Ruslan'},
+  {id:'2', userEmail: 'Ira@gmail.com', userName: 'Ira'},
+  {id:'3', userEmail: 'Lena@gmail.com', userName: 'Lena'},
+]
+
+const NovaPoshta =[
+  {id:'4', userEmail: 'Roma@Gmail.com', userName: 'Roma'},
+  {id:'5', userEmail: 'Igor@gmail.com', userName: 'Igor'},
+  {id:'6', userEmail: 'Lena@gmail.com', userName: 'Lena'},
+]
+
+const UkrPoshta = [
+  {id:'7', userEmail: 'Ola@Gmail.com', userName: 'Ola'},
+  {id:'8', userEmail: 'Marina@gmail.com', userName: 'Marina'},
+  {id:'9', userEmail: 'Gena@gmail.com', userName: 'Gena'},
+]
+
+
 function App() {
 
   return (
     <div>
-      <MailBox boxtitle='Meest Express' mailBoxCount={5} />
-      <MailBox boxtitle='Nova Poshta' mailBoxCount={3} />
-      <MailBox boxtitle='Ukr Poshta' mailBoxCount={0} />
+      <MailBox boxtitle='Meest Express' mailBoxCount={5} boxUsers={MeestExpressUsers} />
+      <MailBox boxtitle='Nova Poshta' mailBoxCount={3} boxUsers={NovaPoshta} />
+      <MailBox boxtitle='Ukr Poshta' mailBoxCount={0} boxUsers={UkrPoshta} />
     </div>
   )
 }
--- src/components/MailBox/MailBox.jsx
+++ src/components/MailBox/MailBox.jsx
@@ -1,13 +1,13 @@
 
-const MailBox = ({boxtitle, mailBoxCount}) => {
+const MailBox = ({boxtitle, mailBoxCount, boxUsers}) => {
   return (
     <div>
       <h2>{boxtitle}</h2>
       {mailBoxCount === 0 ? <p>Сейчас нет активных ячеек</p> : <p>Количество активных ячеек: {mailBoxCount}</p> }
       <ul>
-        <li>Ruslan@Gmail.com</li>
-        <li>Ira@gmail.com</li>
-        <li>Lena@gmail.com</li>
+        {boxUsers.map(user => {
+        return <li key={user.id}>{user.userEmail}</li>
+        })}
       </ul>
     </div>
   )

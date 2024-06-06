5d52550b9a2eada27087c2d819f1644b854ad42e
RuslanGon <ruslan441006@gmail.com>
2024-06-06 13:31:12 +0300

    wer

--- src/components/MailBoxItem/MailBoxItem.jsx
+++ src/components/MailBoxItem/MailBoxItem.jsx
@@ -1,10 +1,12 @@
-
+import css from '../MailBoxItem/MailBoxItem.module.css'
 
 const MailBoxItem = ({user, onDeleteUser}) => {
   return (
     <>
-    <li>email: {user.userEmail}</li>
-    <li>name: {user.userName}</li>
+    <p>email: {user.userEmail}</p>
+    <p className={css.name}>name: <div style={{
+      backgroundColor: user.favColor
+    }} className={css.color}/> {user.userName}</p>
     <button type="button" onClick={() => onDeleteUser(user.id)}>delet user</button>
     </>
   )

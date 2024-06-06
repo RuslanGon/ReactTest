ef6d0fdb1521d4bc9bffb13d0be567d5e2499d98
RuslanGon <ruslan441006@gmail.com>
2024-06-06 12:30:12 +0300

    wer

--- src/components/MailBoxForm/MailBoxForm.jsx
+++ src/components/MailBoxForm/MailBoxForm.jsx
@@ -1,56 +1,45 @@
-import { Field, Form, Formik } from "formik"
 
+import { Formik, Form, Field } from 'formik';
 
-const INITIAL_FORM = {
-  userEmail: "",
-  userName: "",
+const initialValues = {
+  username: "",
+  email: "",
 };
 
-const MailBoxForm = ({onAddUser}) => {
 
-// const handleSubmit = (event) => {
-//     event.preventDefault()
-//     const userEmail = event.currentTarget.elements.userEmail.value
-//     const userName = event.currentTarget.elements.userName.value
-
-//     const formData = {
-//       userEmail,
-//       userName,
-//     };
-    // console.log(formData);
-
-    // onAddUser(formData)
-    
-    // event.currentTarget.reset()
-// }
-
-const handleSubmit = (values, actions) => {
-  onAddUser(values);
-  actions.resetForm();
-};
+const MailBoxForm = ({ onAddUser }) => {
+  const handleSubmit = (values, actions) => {
+    onAddUser(values);
+    actions.resetForm();
+  };
 
   return (
     <div>
-      <Formik initialValues={{INITIAL_FORM}} onSubmit={handleSubmit}>
-        <Form>
-          <h2>Add new email user</h2>
-          <label>
-            <span>User email</span>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        {() => (
+          <Form>
+            <h2>Add new email user</h2>
+            <label>
+              <span>User email</span>
+              <br />
+              <Field type="email" name="userEmail" placeholder="Enter your email" />
+            </label>
             <br />
-            <Field type="email" name="userEmail" placeholder="enter your email" />
-          </label>
-          <br />
-          <label>
-            <span>User name</span>
+            <label>
+              <span>User name</span>
+              <br />
+              <Field type="text" name="userName" placeholder="Enter your name" />
+            </label>
             <br />
-            <Field type="text" name="userName" placeholder="enter your name" />
-          </label>
-          <br />
-          <button type="submit">Add user</button>
-        </Form>
+            <button type="submit">Add user</button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
-}
+};
 
-export default MailBoxForm
+export default MailBoxForm;

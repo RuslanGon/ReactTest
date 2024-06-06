6547afe974310444a41bfcdc4c0f7be1be986cad
RuslanGon <ruslan441006@gmail.com>
2024-06-06 12:45:34 +0300

    wer

--- src/components/MailBoxForm/MailBoxForm.jsx
+++ src/components/MailBoxForm/MailBoxForm.jsx
@@ -6,10 +6,6 @@ import * as yup from 'yup';
 const mailBoxSchema = yup.object({
   firstName: yup.string().defined(),
   nickName: yup.string().default('').nullable(),
-  sex: yup
-    .mixed()
-    .oneOf(['male', 'female', 'other'] as const)
-    .defined(),
   email: yup.string().nullable().email(),
   birthDate: yup.date().nullable().min(new Date(1900, 0, 1)),
 });

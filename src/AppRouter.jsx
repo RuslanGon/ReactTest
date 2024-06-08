ba74429bf9484470d4c1e4d6d1acd76304f19650
RuslanGon <ruslan441006@gmail.com>
2024-06-08 17:49:11 +0300

    wer

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,35 +1,45 @@
-import { NavLink, Route, Routes } from "react-router-dom"
-import css from './AppRouter.module.css'
-import clsx from "clsx"
-import MailBoxPage from "./pages/MailBoxPage"
-import ProductsPage from "./pages/ProductsPage"
-import SearchPage from "./pages/SearchPage"
+import { NavLink, Route, Routes } from "react-router-dom";
+import css from "./AppRouter.module.css";
+import clsx from "clsx";
+import MailBoxPage from "./pages/MailBoxPage";
+import ProductsPage from "./pages/ProductsPage";
+import SearchPage from "./pages/SearchPage";
+import HomePage from "./pages/HomePage";
 
-
-const getLinkStyle = ({isActive}) => clsx(css.navLink, {
-  [css.active] : isActive,
-})
-
+const getLinkStyle = ({ isActive }) =>
+  clsx(css.navLink, {
+    [css.active]: isActive,
+  });
 
 const AppRouter = () => {
   return (
     <div>
       <header>
         <nav className={css.nav}>
-        <NavLink className={getLinkStyle} to='/mailbox'>MailBox</NavLink>
-        <NavLink className={getLinkStyle} to='/products'>Products</NavLink>
-        <NavLink className={getLinkStyle} to='/search'>Search</NavLink>
+          <NavLink className={getLinkStyle} to="/">
+            HomePage
+          </NavLink>
+          <NavLink className={getLinkStyle} to="/mailbox">
+            MailBox
+          </NavLink>
+          <NavLink className={getLinkStyle} to="/products">
+            Products
+          </NavLink>
+          <NavLink className={getLinkStyle} to="/search">
+            Search
+          </NavLink>
         </nav>
       </header>
       <main>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/mailbox" element={<MailBoxPage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/search" element={<SearchPage />} />
         </Routes>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+
+
+const HomePage = () => {
+  return (
+    <h1>Wellcom to home page</h1>
+  )
+}
+
+export default HomePage

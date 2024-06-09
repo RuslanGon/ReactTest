5e735f0f0becc716e8125553739781972e8e9f72
RuslanGon <ruslan441006@gmail.com>
2024-06-09 11:43:03 +0300

    wer

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 import { Link, Route, Routes, useParams } from "react-router-dom";
 import { reguestProductDetailsById } from "../services/api";
 import CommentsPage from "./CommentsPage";
+import Loader from "../components/Loader/Loader";
+import Error from "../components/Error/Error";
 
 
 const ProductDetailsPage = () => {
   const { productId } = useParams();
 
   const [productDetails, setProductDetails] = useState(null);
+  const [isLoader, setIsLoader] = useState(false)
+const [isError, setIsError] =useState(false)
 
   useEffect(() => {
     async function fetchProductDetails() {
       try {
+        setIsLoader(true);
         const data = await reguestProductDetailsById(productId);
         setProductDetails(data);
       } catch (error) {
-        console.error("Error fetching product details:", error);
+        setIsError(true);
+      }finally{
+        setIsLoader(false);
       }
     }
 
@@ -27,6 +34,8 @@ const ProductDetailsPage = () => {
   return (
     <div>
       <h1>Product Details: {productId}</h1>
+      {isLoader && <Loader />}
+        {isError && <Error />}
       {productDetails && (
         <div>
           <img width={350} src={productDetails.thumbnail} alt={productDetails.title} />

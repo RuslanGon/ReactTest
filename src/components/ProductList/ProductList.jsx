bdc90712c34dae7b6da9cb38cfd1cef24b0e3456
RuslanGon <ruslan441006@gmail.com>
2024-06-09 13:18:53 +0300

    wer

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,16 +1,19 @@
 import { Link, useLocation } from "react-router-dom"
 
+import css from '../../AppRouter.module.css'
+
+
 
 const ProductList = ({products}) => {
 
   const location = useLocation()
 
   return (
-    <ul>
+    <ul className={css.divpro}>
       {location.pathname === '/search' && <h2>Search result</h2>}
       {location.pathname === '/products' && <h2>Products</h2>}
            {Array.isArray(products) && products.map(product => {
-            return ( <li key={product.id}>
+            return ( <li  key={product.id}>
                 <img width={350} src={product.thumbnail} alt="" />
                 <h3>Title: {product.title}</h3>
                 <p>Price: {product.price}</p>
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -3,15 +3,16 @@ import Loader from "../components/Loader/Loader";
 import Error from "../components/Error/Error";
 import ProductList from "../components/ProductList/ProductList";
 import { useProductSearch } from "../hooks/useProductSearch";
+import css from '../AppRouter.module.css'
 
-
+ 
 
 const ProductsPage = () => {
 
   const {products, isLoader, isError} = useProductSearch({isSearchPage: false})
 
   return (
-    <div>
+    <div className={css.divprod}>
         <h1>Smart Ukraine Products</h1>
         {isLoader && <Loader />}
         {isError && <Error />}

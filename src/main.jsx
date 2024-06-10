f472cc9f6a1df857604bc048045a8dbe9bda14fd
RuslanGon <ruslan441006@gmail.com>
2024-06-10 16:55:44 +0300

    wer

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
 import AppRouter from './AppRouter.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'  
+import { store } from './redus/store.js'
+
+// import App from './App.jsx'
 // import AppHTTPSearch from './AppHTTPSearch.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter>
-  {/* // <AppHTTPSearch /> */}
-    {/* // <App /> */}
-    <AppRouter />
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* <AppHTTPSearch /> */}
+      {/* <App /> */}
+      <AppRouter />
     </BrowserRouter>
-  // </React.StrictMode>,
+  </Provider>
+  // </React.StrictMode>
 )

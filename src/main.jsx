06ed46bf3c0d654e0b0a5458eb91d3df9eb7112c
RuslanGon <ruslan441006@gmail.com>
2024-06-11 16:24:03 +0300

    wer

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import AppRouter from './AppRouter.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'  
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 // import App from './App.jsx'
 // import AppHTTPSearch from './AppHTTPSearch.jsx'
@@ -12,11 +13,13 @@ import { store } from './redux/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <BrowserRouter>
       {/* <AppHTTPSearch /> */}
       {/* <App /> */}
       <AppRouter />
     </BrowserRouter>
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,35 @@
-
 import { configureStore } from '@reduxjs/toolkit';
 import { mailboxReducer } from './mailbox/mailboxReduser';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
+const mailboxPersistConfig = {
+  key: 'mailbox',
+  storage,
+  whitelist: ['users'], // исправлено на whitelist с маленькой буквы
+};
+
+const persistedMailboxReducer = persistReducer(mailboxPersistConfig, mailboxReducer);
 
 export const store = configureStore({
   reducer: {
-  mailbox: mailboxReducer
-  }
+    mailbox: persistedMailboxReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-
+export const persistor = persistStore(store);

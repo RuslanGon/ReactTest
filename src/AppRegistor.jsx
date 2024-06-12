f01a54cefdf6ca6624c01f5bdacb2d89e8fb7d95
RuslanGon <ruslan441006@gmail.com>
2024-06-12 17:29:23 +0300

    wer

--- src/AppRegistor.jsx
+++ src/AppRegistor.jsx
@@ -1,6 +1,5 @@
-import { NavLink, Route, Routes } from "react-router-dom";
-import css from "./AppRouter.module.css";
-import clsx from "clsx";
+import { Route, Routes } from "react-router-dom";
+
 import { Suspense, lazy } from "react";
 import Loader from "./components/Loader/Loader";
 import RegistrationPage from "./pages/RegistrationPage";
@@ -8,21 +7,12 @@ import LoginPage from "./pages/LoginPage";
 import ContactsPage from "./pages/ContactsPage";
 import Layout from "./components/Layout/Layout";
 
-// import MailBoxPage from "./pages/MailBoxPage";
-// import ProductsPage from "./pages/ProductsPage";
-// import SearchPage from "./pages/SearchPage";
-// import HomePage from "./pages/HomePage";
-// import NotFoundPage from "./pages/NotFoundPage";
-// import ProductDetailsPage from "./pages/ProductDetailsPage";
-
 const MailBoxPage = lazy(() => import('./pages/MailBoxPage'))
 const ProductsPage = lazy(() => import('./pages/ProductsPage'))
 const SearchPage = lazy(() => import("./pages/SearchPage"))
 const HomePage = lazy(() => import('./pages/HomePage'))
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage'))
 const ProductDetailsPage = lazy(() => import('./pages/ProductDetailsPage'))
-
-
 
 const AppRegistor = () => {
   return (

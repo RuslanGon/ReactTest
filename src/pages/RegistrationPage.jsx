a8af15b94198a9bf94678ef266c5c18f5330b089
RuslanGon <ruslan441006@gmail.com>
2024-06-12 17:51:38 +0300

    wer

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,8 +1,75 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+import * as Yup from "yup";
+
+const registorSchema = Yup.object({
+    email: Yup.string().email("enter your email").required("enter your email"),
+    name: Yup.string().required("enter your name"),
+    password: Yup.string().required("enter your password")
+});
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 const RegistrationPage = () => {
-  return (
-    <div>RegistrationPage</div>
-  )
-}
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    // onAddUser(values);
+    actions.resetForm();
+  };
 
-export default RegistrationPage
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={registorSchema}
+      >
+        {() => (
+          <Form>
+            <h2>Registor user</h2>
+            <label>
+              <span>Email:</span>
+              <br />
+              <Field
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+              />
+              <ErrorMessage name="email" component="p" />
+            </label>
+            <br />
+            <label>
+              <span>Name:</span>
+              <br />
+              <Field
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+              />
+              <ErrorMessage name="name" component="p" />
+            </label>
+            <br />
+            <label>
+              <span>Password:</span>
+              <br />
+              <Field
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+              <ErrorMessage name="password" component="p" />
+            </label>
+            <br />
+            <button type="submit">Add user</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default RegistrationPage;

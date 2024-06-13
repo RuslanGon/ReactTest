d7f27df6ac710b8d3735a8d602e5c68ccb62f471
RuslanGon <ruslan441006@gmail.com>
2024-06-13 10:20:25 +0300

    wer

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -37,6 +37,22 @@ const INITIAL_STATE = {
 const authSlice = createSlice({
   name: "auth",
   initialState: INITIAL_STATE,
+  extraReducers: (builder) =>
+    builder
+      .addCase(apiRegistor.pending, (state) => {
+        (state.isLoading = true), 
+        (state.isError = false);
+      })
+      .addCase(apiRegistor.fulfilled, (state, action) => {
+        (state.isLoading = false), 
+        (state.isSignedIn = true);
+        state.userData = action.payload.user;
+        state.token = action.payload.token
+      })
+      .addCase(apiRegistor.rejected, state => {
+        (state.isLoading = false), 
+        (state.isError = true);
+      })
 });
 
 export  const authReducer = authSlice.reducer;
